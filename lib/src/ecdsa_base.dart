d31e6ffde87a47c842fb6b606b3b00b855a2fb5e
YuriiBaryshev <yura.baryshev@gmail.com>
2023-07-13 14:33:25 +0300

    create relevant basic class

--- lib/src/ecdsa_base.dart
+++ lib/src/ecdsa_base.dart
@@ -1,6 +1,26 @@
-// TODO: Put public facing types in this file.
+import 'package:elliptic_curves_facade/elliptic_curves_facade.dart';
+import 'package:elliptic/elliptic.dart';
+import 'dart:math';
 
-/// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
+/// ECDSA implementation
+class ECDSA {
+  late EllipticCurveFacade ellipticCurveFacade;
+  late BigInt _privateKey;
+
+  ECDSA([EllipticCurve? curve]) {
+    curve ??= getSecp256k1() as EllipticCurve;
+    ellipticCurveFacade = EllipticCurveFacade(curve);
+    generatePrivateKey();
+  }
+
+  ///Generate private key
+  void generatePrivateKey() {
+    int length = ellipticCurveFacade.curve.n.bitLength >> 3;
+    Random generator = Random.secure();
+    _privateKey = BigInt.zero;
+    for(int i = 0; i < length; i++) {
+      _privateKey = (_privateKey << 8) + BigInt.from(generator.nextInt(256));
+    }
+    _privateKey = _privateKey % ellipticCurveFacade.curve.n;
+  }
 }
