0938853198890b809c469916e51da6ff89301964
YuriiBaryshev <yura.baryshev@gmail.com>
2023-07-13 15:06:27 +0300

    add signing algorithm

--- lib/src/ecdsa_base.dart
+++ lib/src/ecdsa_base.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:elliptic_curves_facade/elliptic_curves_facade.dart';
 import 'package:elliptic/elliptic.dart';
 import 'dart:math';
@@ -36,5 +38,49 @@ class ECDSA {
       secret = _generateSecret();
     }
     return secret;
+  }
+
+  ///Converts Uint8List to BigInt
+  BigInt _uint8ListToBigInt(Uint8List list) {
+    BigInt output = BigInt.zero;
+    for(int i = 0; i < list.length; i++) {
+      output = output << 8;
+      output = output + BigInt.from(list[i]);
+    }
+    return output;
+  }
+
+  ///Signing message
+  ///Mapping/JSON of the following structure is outputted
+  ///{
+  ///   "r": <something> as BigInt
+  ///   "s": <something> as BigInt
+  ///}
+  Map<String, BigInt> sign(Uint8List message) {
+    Map<String, BigInt> output = {
+      "r": BigInt.zero,
+      "s": BigInt.zero,
+    };
+
+    Uint8List hashValue = _hashFunction.process(message);
+    BigInt e = _uint8ListToBigInt(hashValue);
+    BigInt k = _generateSecret();
+    BigInt kInv = k.modInverse(ellipticCurveFacade.curve.n);
+    ECPoint R = ellipticCurveFacade.mulScalar(ellipticCurveFacade.getG(), k);
+
+    BigInt r = R.x % ellipticCurveFacade.curve.n;
+    BigInt s = kInv * (e + (r * k) % ellipticCurveFacade.curve.n)
+        % ellipticCurveFacade.curve.n;
+
+    k = BigInt.zero;
+    kInv = BigInt.zero;
+
+    if((r == BigInt.zero) || (s == BigInt.zero)) {
+      output = sign(message);
+    } else {
+      output["s"] = s;
+      output["r"] = r;
+    }
+    return output;
   }
 }
