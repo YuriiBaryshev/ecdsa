80b66d67eecfceb4a078a6d0f5f467231380d763
YuriiBaryshev <yura.baryshev@gmail.com>
2023-07-13 12:38:42 +0300

    create project's carcass

---
+++ example/ecdsa_example.dart
@@ -0,0 +1,6 @@
+import 'package:ecdsa/ecdsa.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/ecdsa.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library ecdsa;
+
+export 'src/ecdsa_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
