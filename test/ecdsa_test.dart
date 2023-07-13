79be6a3a68e69bf418ea16eff2d30021d6f0204b
YuriiBaryshev <yura.baryshev@gmail.com>
2023-07-13 16:02:16 +0300

    add signing and verification compatibility test cases

--- test/ecdsa_test.dart
+++ test/ecdsa_test.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:ecdsa/ecdsa.dart';
 import 'package:elliptic/elliptic.dart';
 import 'package:test/test.dart';
@@ -5,6 +7,8 @@ import 'package:test/test.dart';
 void main() {
   group('ECDSA tests', () {
     ECDSA ecdsa = ECDSA();
+    Uint8List message = Uint8List.fromList([0, 1, 2, 3 , 42]);
+    
 
     setUp(() {
       // Additional setup goes here.
@@ -12,6 +16,13 @@ void main() {
 
     test('Secp256k1 is set default', () {
       expect(ecdsa.ellipticCurveFacade.curve, getSecp256k1());
+    });
+
+    test('signing is compatible with verification', () {
+      for(int i = 0; i < 10; i++) {
+        var signature = ecdsa.sign(message);
+        expect(ecdsa.verify(message, signature), isTrue);
+      }
     });
   });
 }
