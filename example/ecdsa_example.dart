2fefc14e9a6d1c98b68d5f3e524fe675c662aeb4
YuriiBaryshev <yura.baryshev@gmail.com>
2023-07-13 16:58:04 +0300

    add usage example

--- example/ecdsa_example.dart
+++ example/ecdsa_example.dart
@@ -1,6 +1,14 @@
+import 'dart:typed_data';
+
 import 'package:ecdsa/ecdsa.dart';
+import 'package:elliptic_curves_facade/elliptic_curves_facade.dart';
 
 void main() {
-  ECDSA ecdsa = ECDSA();
-  print('${ecdsa.ellipticCurveFacade}');
+  ECDSA ecdsaAlice = ECDSA();
+  Uint8List message = Uint8List.fromList(([0x61, 0x62, 0x63])); //a, b, c
+  var signature = ecdsaAlice.sign(message);
+  ECPoint alicePublicKey = ecdsaAlice.publicKey;
+  //sending signature and alicePublicKey
+  ECDSA ecdsaBob = ECDSA();
+  print('${ecdsaBob.verify(message, signature, alicePublicKey)}'); //true
 }
